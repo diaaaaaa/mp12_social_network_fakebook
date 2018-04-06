fbd184b27405ca95f8823e6dd0db1125cf983885
diaaaaaa <ite.diaa@gmail.com>
2018-04-06 12:55:23 +0200

    Social Network Fakebook

---
+++ src/main/java/at/refugeescode/mp12_social_network_fakebook/endpoint/PersonEndPoint.java
@@ -0,0 +1,4 @@
+package at.refugeescode.mp12_social_network_fakebook.endpoint;
+
+public class PersonEndPoint {
+}
---
+++ src/main/java/at/refugeescode/mp12_social_network_fakebook/persistence/Person.java
@@ -0,0 +1,4 @@
+package at.refugeescode.mp12_social_network_fakebook.persistence;
+
+public class Person {
+}
